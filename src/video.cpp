83ad46cbbc76cba9bb33d03865a3a435cf54623b
paja <janusch.patas@gmx.de>
2022-06-28 19:48:11 +0200

    ffmpeg lib can be created

--- src/video.cpp
+++ src/video.cpp
@@ -4,10 +4,17 @@
 
 extern "C" {
 #include <libavcodec/avcodec.h>
+#include <libavutil/frame.h>
+#include <libavcodec/codec.h>
 }
 
-int main() {
-    RcOverride rc;
-    rc.end_frame = 1;
-    rc.start_frame = 1;
+
+int main(int argc, char **argv) {
+
+    AVCodecContext *dec_ctx;
+    AVFrame *frame = new AVFrame;
+    frame->width = 1;
+    avcodec_receive_frame(dec_ctx, frame);
+
+    return 0;
 }
